06a4f7922a8ceaa26d6f4c2256a5be16697599a0
chomusuke-png <owokenji@gmail.com>
2026-02-27 17:10:10 -0300

    persistent_bottom_nav_bar pasado a google_nav_bar

--- lib/screens/main_wrapper.dart
+++ lib/screens/main_wrapper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:chilehalal_mobile/screens/home/home_screen.dart';
 import 'package:chilehalal_mobile/screens/catalog/catalog_screen.dart';
 import 'package:chilehalal_mobile/screens/scanner/scanner_screen.dart';
@@ -17,85 +17,95 @@ class MainWrapper extends StatefulWidget {
 }
 
 class MainWrapperState extends State<MainWrapper> {
-  final PersistentTabController _controller = PersistentTabController(initialIndex: 0);
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    CatalogScreen(),
+    ScannerScreen(),
+    FavoritesScreen(),
+    AccountScreen(),
+  ];
 
   void jumpToTab(int index, {Widget? screenToPush}) {
-    _controller.jumpToTab(index);
-    if (screenToPush != null && mounted) {
-      Future.delayed(const Duration(milliseconds: 100), () {
-        Navigator.of(context).push(MaterialPageRoute(builder: (_) => screenToPush));
+    if (mounted) {
+      setState(() {
+        _selectedIndex = index;
       });
+      
+      if (screenToPush != null) {
+        Future.delayed(const Duration(milliseconds: 100), () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (_) => screenToPush));
+        });
+      }
     }
-  }
-
-  List<Widget> _buildScreens() {
-    return const [
-      HomeScreen(),
-      CatalogScreen(),
-      ScannerScreen(),
-      FavoritesScreen(),
-      AccountScreen(),
-    ];
-  }
-
-  List<PersistentBottomNavBarItem> _navBarsItems(ColorScheme colorScheme) {
-    return [
-      PersistentBottomNavBarItem(
-        icon: const FaIcon(FontAwesomeIcons.solidHouse),
-        title: "Inicio",
-        activeColorPrimary: colorScheme.primary,
-        inactiveColorPrimary: Colors.grey,
-        activeColorSecondary: Colors.white,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const FaIcon(FontAwesomeIcons.list),
-        title: "Catálogo",
-        activeColorPrimary: colorScheme.primary,
-        inactiveColorPrimary: Colors.grey,
-        activeColorSecondary: Colors.white,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.barcode_reader),
-        title: "Escanear",
-        activeColorPrimary: colorScheme.primary,
-        inactiveColorPrimary: Colors.grey,
-        activeColorSecondary: Colors.white,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const FaIcon(FontAwesomeIcons.solidHeart),
-        title: "Favoritos",
-        activeColorPrimary: colorScheme.primary,
-        inactiveColorPrimary: Colors.grey,
-        activeColorSecondary: Colors.white,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const FaIcon(FontAwesomeIcons.solidUser),
-        title: "Cuenta",
-        activeColorPrimary: colorScheme.primary,
-        inactiveColorPrimary: Colors.grey,
-        activeColorSecondary: Colors.white,
-      ),
-    ];
   }
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return PersistentTabView(
-      context,
-      controller: _controller,
-      screens: _buildScreens(),
-      items: _navBarsItems(colorScheme),
-      backgroundColor: colorScheme.surface,
-      handleAndroidBackButtonPress: true,
-      resizeToAvoidBottomInset: true,
-      stateManagement: true, 
-      decoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        colorBehindNavBar: colorScheme.surface,
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
       ),
-      navBarStyle: NavBarStyle.style7,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: colorScheme.surface,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withValues(alpha: 0.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: colorScheme.primary,
+              iconSize: 22,
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+              duration: const Duration(milliseconds: 200),
+              tabBackgroundColor: colorScheme.primary.withValues(alpha: 0.1),
+              color: Colors.grey[600],
+              tabs: const [
+                GButton(
+                  icon: FontAwesomeIcons.solidHouse,
+                  text: 'Inicio',
+                ),
+                GButton(
+                  icon: FontAwesomeIcons.list,
+                  text: 'Catálogo',
+                ),
+                GButton(
+                  icon: Icons.barcode_reader,
+                  iconSize: 26,
+                  text: 'Escanear',
+                ),
+                GButton(
+                  icon: FontAwesomeIcons.solidHeart,
+                  text: 'Favoritos',
+                ),
+                GButton(
+                  icon: FontAwesomeIcons.solidUser,
+                  text: 'Cuenta',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
